abdf4ec9acf434511ae86a8492688d6d0844f6b3
NAVEEN KUMAR <nk946432@gmail.com>
2025-09-02 11:49:42 +0530

    changed the frontend url

--- frontend/src/components/FacultyLogin.jsx
+++ frontend/src/components/FacultyLogin.jsx
@@ -16,7 +16,7 @@ const FacultyLogin = ({ setShowFacultyLogin }) => {
   const sections = ['A', 'B', 'C', 'D'];
 
   const pass=import.meta.env.VITE_PASS;
-  const API_URL=import.meta.env.VITE_API_URL || 'http://localhost:5000';
+  const API_URL=import.meta.env.VITE_URL || 'http://localhost:5000';
 
   const handlePasswordSubmit = async () => {
     if (password === pass) {
